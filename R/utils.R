4fb391a0bb984fdd62fb3d45bfa91b3da79e6c9a
Teal Emery <ltemery@gmail.com>
2025-01-25 14:02:08 -0500

    Add minimal R function + test
    Added function + test to pass R CMD Check  CI + Codecov CI

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+#' Add two numbers
+#'
+#' @param x A number
+#' @param y A number
+#' @return The sum of x and y
+#' @export
+#'
+#' @examples
+#' add_numbers(1, 2)
+add_numbers <- function(x, y) {
+  x + y
+}
---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,3 @@
+test_that("add_numbers works", {
+  expect_equal(add_numbers(1, 2), 3)
+})
